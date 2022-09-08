c92a68f64037cbc0e3eaab260e7a771c6b238c0c
Shibo Lyu <github@of.sb>
2022-09-08 16:45:12 +0800

    v0.1.0.

--- Sources/EasyChanges/EasyChanges.swift
+++ Sources/EasyChanges/EasyChanges.swift
@@ -1,6 +1,60 @@
-public struct EasyChanges {
-    public private(set) var text = "Hello, World!"
+public typealias EasyChangeElement = Equatable & Identifiable & Hashable
 
-    public init() {
+public enum EasyChange<Element: EasyChangeElement>: Equatable, Hashable {
+    case insert(offset: Int, element: Element)
+    case remove(offset: Int, element: Element)
+    case move(from: Int, to: Int, element: Element)
+    case change(offset: Int, oldElement: Element, newElement: Element)
+}
+
+extension CollectionDifference where ChangeElement: EasyChangeElement {
+    public func easyChanges() -> [EasyChange<ChangeElement>] {
+        var changes: [EasyChange<ChangeElement>] = []
+        var removeOffsetsById: [ChangeElement.ID: Int] = [:]
+        var oldElementsById: [ChangeElement.ID: ChangeElement] = [:]
+        var removeIds: [ChangeElement.ID] = []
+        
+        for change in self.removals.reversed() {
+            guard case let .remove(offset, element, associatedWith) = change else { fatalError() }
+            if let newPosition = associatedWith {
+                changes.append(.move(from: offset, to: newPosition, element: element))
+            } else {
+                removeOffsetsById[element.id] = offset
+                oldElementsById[element.id] = element
+                removeIds.append(element.id)
+            }
+        }
+        
+        for change in self.insertions {
+            guard case let .insert(offset, element, associatedWith) = change else { fatalError() }
+            if associatedWith != nil { continue }
+            if
+                let prevOffset = removeOffsetsById[element.id],
+                let oldElement = oldElementsById[element.id]
+            {
+                if prevOffset != offset {
+                    changes += [
+                        .change(offset: prevOffset, oldElement: oldElement, newElement: element),
+                        .move(from: prevOffset, to: offset, element: element)
+                    ]
+                } else {
+                    changes.append(
+                        .change(offset: offset, oldElement: oldElement, newElement: element)
+                    )
+                }
+                
+                removeOffsetsById[element.id] = nil
+                oldElementsById[element.id] = nil
+                removeIds.remove(at: removeIds.firstIndex(of: element.id)!)
+            } else {
+                changes.append(.insert(offset: offset, element: element))
+            }
+        }
+        
+        let removeChanges = removeIds.map { id in
+            EasyChange.remove(offset: removeOffsetsById[id]!, element: oldElementsById[id]!)
+        }
+        
+        return removeChanges + changes
     }
 }
--- Tests/EasyChangesTests/EasyChangesTests.swift
+++ Tests/EasyChangesTests/EasyChangesTests.swift
@@ -1,11 +1,111 @@
 import XCTest
 @testable import EasyChanges
 
+struct TestElement: Identifiable, Equatable, Hashable {
+    var id: String
+    var content: String
+}
+
+let a = TestElement(id: "A", content: "Hello, A!")
+let b = TestElement(id: "B", content: "Hello, B!")
+let c = TestElement(id: "C", content: "Hello, C!")
+let d = TestElement(id: "D", content: "Hello, D!")
+
+let bm = TestElement(id: "B", content: "Hello, B!!!")
+let cm = TestElement(id: "C", content: "Hello, C!!!")
+let dm = TestElement(id: "D", content: "Hello, D!!!")
+
+typealias Elements = [TestElement]
+typealias Changes = [EasyChange<TestElement>]
+
+func runEasyChanges(from old: Elements, to new: Elements) -> Changes {
+    new.difference(from: old).inferringMoves().easyChanges()
+}
+
 final class EasyChangesTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(EasyChanges().text, "Hello, World!")
+    func testAppend() throws {
+        let old = [a, b]
+        let new = [a, b, c]
+        
+        let expectedChanges: Changes = [.insert(offset: 2, element: c)]
+        
+        XCTAssertEqual(runEasyChanges(from: old, to: new), expectedChanges)
+    }
+    func testAppendMultiple() throws {
+        let old = [a]
+        let new = [a, b, c]
+        
+        let expectedChanges: Changes = [
+            .insert(offset: 1, element: b),
+            .insert(offset: 2, element: c)
+        ]
+        
+        XCTAssertEqual(runEasyChanges(from: old, to: new), expectedChanges)
+    }
+    func testInsert() throws {
+        let old = [a, c]
+        let new = [a, b, c]
+        
+        let expectedChanges: Changes = [.insert(offset: 1, element: b)]
+        
+        XCTAssertEqual(runEasyChanges(from: old, to: new), expectedChanges)
+    }
+    func testInsertMultiple() throws {
+        let old = [a, d]
+        let new = [a, b, c, d]
+        
+        let expectedChanges: Changes = [
+            .insert(offset: 1, element: b),
+            .insert(offset: 2, element: c)
+        ]
+        
+        XCTAssertEqual(runEasyChanges(from: old, to: new), expectedChanges)
+    }
+    func testRemove() throws {
+        let old = [a, b, c]
+        let new = [a, b]
+        
+        let expectedChanges: Changes = [.remove(offset: 2, element: c)]
+        
+        XCTAssertEqual(runEasyChanges(from: old, to: new), expectedChanges)
+    }
+    func testRemoveMultiple() throws {
+        let old = [a, b, c]
+        let new = [a]
+        
+        // Must use this order, or out of bounds errors will happen.
+        let expectedChanges: Changes = [
+            .remove(offset: 2, element: c),
+            .remove(offset: 1, element: b)
+        ]
+        
+        XCTAssertEqual(runEasyChanges(from: old, to: new), expectedChanges)
+    }
+    func testMove() throws {
+        let old = [a, b, c]
+        let new = [b, c, a]
+        
+        let expectedChanges: Changes = [.move(from: 0, to: 2, element: a)]
+        
+        XCTAssertEqual(runEasyChanges(from: old, to: new), expectedChanges)
+    }
+    func testChange() throws {
+        let old = [a, b, c]
+        let new = [a, bm, c]
+        
+        let expectedChanges: Changes = [.change(offset: 1, oldElement: b, newElement: bm)]
+        
+        XCTAssertEqual(runEasyChanges(from: old, to: new), expectedChanges)
+    }
+    func testMoveAndChange() throws {
+        let old = [a, b, c, d]
+        let new = [dm, a, b, c]
+        
+        let expectedChanges: Changes = [
+            .change(offset: 3, oldElement: d, newElement: dm),
+            .move(from: 3, to: 0, element: dm)
+        ]
+        
+        XCTAssertEqual(runEasyChanges(from: old, to: new), expectedChanges)
     }
 }
